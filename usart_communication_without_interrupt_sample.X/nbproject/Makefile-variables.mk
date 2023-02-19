d482cd918d4c77be785e727b43f2d44d26998068
Luiz Fernando M. Arruda <55357138+lfmarruda89@users.noreply.github.com>
2023-02-19 20:45:34 -0300

    Adição de Projetos Base

---
+++ usart_communication_without_interrupt_sample.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=usart_communication_without_interrupt_sample.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/usart_communication_without_interrupt_sample.X.production.hex
