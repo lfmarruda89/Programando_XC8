ea4354cc88197c2b93e9c8a37586b0cc25495ad2
Luiz Fernando M. Arruda <ENG.LUIZARRUDA@GMAIL.COM>
2023-02-20 13:54:07 -0300

    Adição de Projetos Base

--- LCD_sample.X/main.c
+++ LCD_sample.X/main.c
@@ -1,37 +1,31 @@
 #define _XTAL_FREQ 20000000
 
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "main.h"
 #include "lcd.h"
 
 void main ( void ) {
 
     LCD lcd = { &PORTD, 2, 3, 4, 5, 6, 7}; // PORT, RS, EN, D4, D5, D6, D7
-
     LCD_Init(lcd);
 
-    for ( ;; ) {
-
-        LCD_Clear();
-        LCD_Set_Cursor(0,0);
-        LCD_putrs("HELLO WORLD!");
-
+    LCD_Clear();
+    LCD_Set_Cursor(0,0);
+    LCD_putrs("LUIZ FERNANDO");
+    while (1){
 		LCD_Set_Cursor(1,0);
-		for ( char c = 'A'; c <= 'P'; c++ ) { // Print characters A-P
+		for ( char c = 'A'; c <= 'P'; c++ ) { 
 			 LCD_putc(c);
-			 for ( unsigned char i = 0; i < 30; i++ ) { // 300ms Delay
+			 for ( unsigned char i = 0; i < 30; i++ ) { 
 			 	__delay_ms(10);
 			 }
 		}
-		
-		for ( unsigned char i = 0; i < 100; ++i ) { // 1s Delay
+		for ( unsigned char i = 0; i < 100; ++i ) { 
 		 	__delay_ms(10);
 		 }
-
 	}
-
     return;
 }
 
